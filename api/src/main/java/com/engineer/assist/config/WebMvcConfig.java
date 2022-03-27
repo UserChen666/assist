7b399748b244a7ed9848902939faf50117506ac3
wangbintao <541437863@qq.com>
2022-03-27 18:54:20 +0800

    跨域

--- api/src/main/java/com/engineer/assist/config/WebMvcConfig.java
+++ api/src/main/java/com/engineer/assist/config/WebMvcConfig.java
@@ -1,13 +1,24 @@
 package com.engineer.assist.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*")
+                // 是否允许证书 不再默认开启
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -33,7 +44,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 "/swagger-resources",
                 "/swagger-resources/configuration/security",
                 "/swagger-ui.html"
-
         );
     }
 }
