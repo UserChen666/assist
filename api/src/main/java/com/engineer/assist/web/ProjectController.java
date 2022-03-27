e89588c9493bec2844f3b8d70b0a362756374a97
wangbintao <541437863@qq.com>
2022-03-27 23:19:00 +0800

    跨域

--- api/src/main/java/com/engineer/assist/web/ProjectController.java
+++ api/src/main/java/com/engineer/assist/web/ProjectController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/projectInfo")
+@RequestMapping("/api/projectInfo")
 @CrossOrigin
 public class ProjectController {
     @Autowired
--- api/src/main/java/com/engineer/assist/web/UserController.java
+++ api/src/main/java/com/engineer/assist/web/UserController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/api/account")
 @CrossOrigin
 public class UserController {
 
