07657f3d979767bb9afcdb9f6831c98084efcc21
wangbintao <541437863@qq.com>
2022-03-27 20:32:24 +0800

    跨域

--- api/src/main/java/com/engineer/assist/config/SwaggerConfig.java
+++ api/src/main/java/com/engineer/assist/config/SwaggerConfig.java
@@ -1,14 +1,17 @@
 package com.engineer.assist.config;
 
+import com.google.common.collect.Lists;
+import io.swagger.models.parameters.Parameter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +21,18 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.engineer.assist.web"))
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build().globalRequestParameters(getHeader());
+    }
+
+    private List<RequestParameter> getHeader() {
+        ArrayList<RequestParameter> h = Lists.newArrayList();
+
+        RequestParameterBuilder requestParameterBuilder = new RequestParameterBuilder();
+        requestParameterBuilder.in(ParameterType.HEADER)
+                .name("token").query(p -> p.defaultValue("assist").allowEmptyValue(false));
+
+        h.add(requestParameterBuilder.build());
+        return h;
     }
 
     private ApiInfo apiInfo(){
