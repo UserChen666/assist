3c7336147f9bddd86a9f2b33b814fb2a47fba3eb
wangbintao <541437863@qq.com>
2022-04-07 22:18:11 +0800

    增加接口

--- common/src/main/java/com/engineer/assist/enumDTO/DataType.java
+++ common/src/main/java/com/engineer/assist/enumDTO/DataType.java
@@ -1,25 +1,21 @@
 package com.engineer.assist.enumDTO;
 
-import com.baomidou.mybatisplus.annotation.IEnum;
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-import java.io.Serializable;
-
-public enum DataType implements IEnum {
+public enum DataType{
 
     CAD(0,"CAD"),
     PDF(1,"PDF"),
     WORD(2,"WORD");
 
+    @EnumValue
+    @JsonValue
     private int code;
     private String name;
 
     DataType(int code, String name) {
         this.code = code;
         this.name = name;
-    }
-
-    @Override
-    public Serializable getValue() {
-        return this.code;
     }
 }
--- common/src/main/java/com/engineer/assist/enumDTO/ProjectType.java
+++ common/src/main/java/com/engineer/assist/enumDTO/ProjectType.java
@@ -2,25 +2,22 @@ package com.engineer.assist.enumDTO;
 
 import com.baomidou.mybatisplus.annotation.EnumValue;
 import com.baomidou.mybatisplus.annotation.IEnum;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.io.Serializable;
 
-public enum ProjectType implements IEnum {
+public enum ProjectType{
 
     CAD(0,"CAD"),
     PDF(1,"PDF"),
     WORD(2,"WORD");
-
+    @EnumValue
+    @JsonValue
     private int code;
     private String name;
 
     ProjectType(int code, String name) {
         this.code = code;
         this.name = name;
-    }
-
-    @Override
-    public Serializable getValue() {
-        return this.code;
     }
 }
