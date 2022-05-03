ffda5b65a4ba78275caf056476ec4ef4b8e0bdf1
wangbintao <541437863@qq.com>
2022-05-03 22:47:48 +0800

    增加接口

--- api/src/main/java/com/engineer/assist/config/WebMvcConfig.java
+++ api/src/main/java/com/engineer/assist/config/WebMvcConfig.java
@@ -16,11 +16,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // 设置允许跨域的路径
         registry.addMapping("/**")
                 // 设置允许跨域请求的域名
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 // 是否允许证书 不再默认开启
-                .allowCredentials(false)
+                .allowCredentials(true)
                 // 设置允许的方法
-                .allowedMethods("*")
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE","OPTIONS"})
+                .allowedHeaders("*")
                 // 跨域允许时间
                 .maxAge(3600);
     }
